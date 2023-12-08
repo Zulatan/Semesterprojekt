8a737ebd7c31af15c958fd935716643ce99ab2dd
Kevin <kevinpede98@gmail.com>
2023-12-08 12:09:43 +0100

    Formået at hente data, mangler styling.

--- abonnementzonen/src/services/database.ts
+++ abonnementzonen/src/services/database.ts
@@ -1,17 +1,33 @@
-import { initializeApp } from 'firebase/app';
-import { environment } from '../environments/environment';
+/*import { initializeApp } from 'firebase/app';
+import { environment } from '../environments/environment';*/
 
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-
 @Injectable({
     providedIn: 'root',
-})
+  })
+  export class SubscriptionService {
+    private apiUrl = 'http://localhost:8080/api/subscription'; // Adjust the URL to your MySQL backend
+  
+    constructor(private http: HttpClient) {}
+  
+    getAllSubscriptions(): Observable<any[]> {
+      return this.http.get<any[]>(`${this.apiUrl}`);
+    }
+  }
+
+
+/*@Injectable({
+    providedIn: 'root',
+})*/
+
+/*export class SubscriptionService {
+private app = initializeApp(environment.firebaseConfig);
 
 export class SubscriptionService {
-private app = initializeApp(environment.firebaseConfig);
+    private apiUrl = 'http://localhost:8080/api/subscription';
 
 private apiUrl = 'http://localhost:8080/api';
 
@@ -20,4 +36,5 @@ constructor(private http: HttpClient) {}
 getSubscriptionOptions(): Observable<string[]> {
     return this.http.get<string[]>(this.apiUrl);
 }
-}
+}*/
+
