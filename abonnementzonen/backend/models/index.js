1762926542cd0a24643d9e5e9682fc089a95e966
Hannah Clausen <hannahdk00@gmail.com>
2023-12-14 10:31:37 +0100

    kamera funktion working

--- abonnementzonen/backend/models/index.js
+++ abonnementzonen/backend/models/index.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize("abonnementzonendb", "root", "", {
   host: "localhost",
   dialect: "mysql",
   port: 3306,
- /*  password: process.env.SECRET, */
+  password: process.env.SECRET, 
 });
 
 const db = {};
@@ -20,13 +20,28 @@ db.subscription = require("./subscription.model.js")(sequelize, Sequelize);
 
 //relation between models (tables)
 db.user.hasMany(db.subscription, {
-    foreignKey: "user_id",
-    as: "user",
-    onDelete: "cascade", //delete all subscription if user is deleted
-  });
-  db.subscription.belongsTo(db.user, {
-    foreignKey: "user_id",
-    as: "user",
-  });
+  foreignKey: "user_id",
+  as: "subscriptions", // Note the change in alias                                   - changed
+  onDelete: "cascade", // Delete all subscriptions if user is deleted 
+});
+
+// Define the relationship between Subscription and Payment
+db.subscription.belongsTo(db.payment, {
+  foreignKey: "payment_id", // Use payment_id as the foreign key in subscription  - changed
+  as: "payment", // Singular alias since it's a one-to-one relationship            - changed
+  onDelete: "cascade", // Delete payment if the associated subscription is deleted
+});
+
+// Define the relationship between Subscription and User
+db.subscription.belongsTo(db.user, {
+  foreignKey: "user_id",
+  as: "user",
+});
+
+// Define the relationship between Payment and Subscription
+db.payment.hasOne(db.subscription, {
+  foreignKey: "payment_id", // Use payment_id as the foreign key in payment                 - changed
+  as: "subscription",
+});
 
 module.exports = db;
